b561ef1acc8cf4d13fe00e004934c0386bf754e5
Rickard Eriksson <rickard.eriksson@gigabit.nu>
2017-01-18 14:27:03 +0100

    pep8-ified the code, except for the ipv6 regex...

--- setup.py
+++ setup.py
@@ -1,23 +1,20 @@
 try:
-  from setuptools import setup
+    from setuptools import setup
 except ImportError:
-  from distutils.core import setup
+    from distutils.core import setup
 
 files = ["config/*"]
 
-setup(name = "dyndd",
-    version = "0.1",
-    description = "Dynamic DNS Daemon",
-    author = "_nalle",
-    author_email = "rickard.eriksson@gigabit.nu",
-    url = "https://www.dyndd.xyz",
-    packages = ['dyndd'],
-    package_data = {'dyndd' : files, 'dyndd/ipaddr': files, 'dyndd/controller': files},
-    long_description = """DNS Daemon that takes records from a mysql database and returns them on request""",
-    install_requires = ['psutil','setproctitle','twisted','MySQL-python']
-)
-
-
-
-#    scripts = ["bin/dyndd"],
-#    data_files = [('/etc/dyndd', ['config/dyndd.conf']), ('/etc/init.d',['startupscripts/dyndd']),('/etc/logrotate.d',['logrotate.d/dyndd'])],
+setup(name="dyndd",
+      version="0.1",
+      description="Dynamic DNS Daemon",
+      author="_nalle",
+      author_email="rickard.eriksson@gigabit.nu",
+      url="https://www.dyndd.xyz",
+      packages=['dyndd'],
+      package_data={'dyndd': files, 'dyndd/ipaddr': files,
+                    'dyndd/controller': files},
+      long_description="""DNS Daemon that takes records from a mysql
+                          database and returns them on request""",
+      install_requires=['psutil', 'setproctitle', 'twisted', 'MySQL-python']
+      )
